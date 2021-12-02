7f59441f94034f456458cf16d213b6e5b2d662d9
bootstrapdash-team <git@bootstrapdash.com>
2021-12-02 16:22:36 +0530

    bootstrap5 initial commit

--- template/js/template.js
+++ template/js/template.js
@@ -109,5 +109,18 @@
         }
       }
     });
+    if ($.cookie('kapella-free-banner')!="true") {
+      document.querySelector('#proBanner').classList.add('d-flex');
+    }
+    else {
+      document.querySelector('#proBanner').classList.add('d-none');
+    }
+    document.querySelector('#bannerClose').addEventListener('click',function() {
+      document.querySelector('#proBanner').classList.add('d-none');
+      document.querySelector('#proBanner').classList.remove('d-flex');
+      var date = new Date();
+      date.setTime(date.getTime() + 24 * 60 * 60 * 1000); 
+      $.cookie('kapella-free-banner', "true", { expires: date });
+    });
   });
 })(jQuery);
